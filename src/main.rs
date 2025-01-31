4362184500a5d2c10693ba289272c17089c86e47
Enfernuz <github.colog@aleeas.com>
2025-01-31 15:07:04 +0400

    [Refactoring] Move request handling logic to the 'handle_request' function

--- src/main.rs
+++ src/main.rs
@@ -45,75 +45,75 @@ fn read_data<const N: usize>(stream: &mut TcpStream) -> Result<(usize, [u8; N]),
     }
 }
 
+fn handle_request(req: &Request) -> Response {
+    let status: Status;
+    let content: Option<Content>;
+    let request_path = req.get_path();
+    if request_path.eq("/") {
+        status = Status::Ok;
+        content = None;
+    } else if request_path.eq("/user-agent") {
+        status = Status::Ok;
+        content = Some(Content {
+            content_type: "text/plain".to_string(),
+            body: req.get_headers().get("User-Agent").unwrap().to_owned(),
+        });
+    } else if request_path.starts_with("/echo/") {
+        status = Status::Ok;
+        content = Some(Content {
+            content_type: "text/plain".to_string(),
+            body: request_path.trim_start_matches("/echo/").to_string(),
+        });
+    } else if request_path.starts_with("/files/") {
+        let root_dir = std::env::args()
+            .nth(2)
+            .expect("Could not read the `--directory` flag value.");
+        let filename = request_path.trim_start_matches("/files/");
+        let path: String = root_dir + filename;
+        let file_content = fs::read_to_string(&path);
+        match file_content {
+            Ok(_content) => {
+                status = Status::Ok;
+                content = Some(Content {
+                    content_type: "application/octet-stream".to_string(),
+                    body: _content,
+                })
+            }
+            Err(e) => {
+                dbg!("Error when reading file at {}: {:?}", &path, &e);
+                status = Status::NotFound;
+                content = None;
+            }
+        }
+    } else {
+        status = Status::NotFound;
+        content = None;
+    }
+
+    let mut headers: HashMap<String, String> = HashMap::new();
+    if let Some(_content) = content.as_ref() {
+        headers.insert("Content-Type".to_string(), _content.content_type.clone());
+        headers.insert(
+            "Content-Length".to_string(),
+            _content.body.len().to_string(),
+        );
+    }
+
+    Response {
+        http_version: req.get_http_version().to_owned(),
+        status: status,
+        headers: headers,
+        content: content,
+    }
+}
+
 fn handle_connection(mut stream: TcpStream) {
     let (bytes_read, buf) =
         read_data::<BUF_SIZE>(&mut stream).expect("Failed to read data from stream.");
     if bytes_read > 0 {
         let req =
             Request::from_raw(&buf[..bytes_read]).expect("Failed to read request from raw input.");
-        dbg!("{:#?}", &req);
-
-        let status: Status;
-        let content: Option<Content>;
-        let request_path = req.get_path();
-        if request_path.eq("/") {
-            status = Status::Ok;
-            content = None;
-        } else if request_path.eq("/user-agent") {
-            status = Status::Ok;
-            content = Some(Content {
-                content_type: "text/plain".to_string(),
-                body: req.get_headers().get("User-Agent").unwrap().to_owned(),
-            });
-        } else if request_path.starts_with("/echo/") {
-            status = Status::Ok;
-            content = Some(Content {
-                content_type: "text/plain".to_string(),
-                body: request_path.trim_start_matches("/echo/").to_string(),
-            });
-        } else if request_path.starts_with("/files/") {
-            let root_dir = std::env::args()
-                .nth(2)
-                .expect("Could not read the `--directory` flag value.");
-            let filename = request_path.trim_start_matches("/files/");
-            let path: String = root_dir + filename;
-            let file_content = fs::read_to_string(&path);
-            match file_content {
-                Ok(_content) => {
-                    status = Status::Ok;
-                    content = Some(Content {
-                        content_type: "application/octet-stream".to_string(),
-                        body: _content,
-                    })
-                }
-                Err(e) => {
-                    dbg!("Error when reading file at {}: {:?}", &path, &e);
-                    status = Status::NotFound;
-                    content = None;
-                }
-            }
-        } else {
-            status = Status::NotFound;
-            content = None;
-        }
-
-        let mut headers: HashMap<String, String> = HashMap::new();
-        if let Some(_content) = content.as_ref() {
-            headers.insert("Content-Type".to_string(), _content.content_type.clone());
-            headers.insert(
-                "Content-Length".to_string(),
-                _content.body.len().to_string(),
-            );
-        }
-
-        let res: Response = Response {
-            http_version: req.get_http_version().to_owned(),
-            status: status,
-            headers: headers,
-            content: content,
-        };
-        dbg!("{:#?}", &res);
-
+        let res = handle_request(&req);
         stream
             .write(res.to_string().as_bytes())
             .expect("Failed to write to the incoming connection's stream.");
